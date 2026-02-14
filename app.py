d0bd2bb25380d0041e0faf502b3583c55c83d7ca
Yendluri Raghava <167185543+Raghava1712@users.noreply.github.com>
2026-02-15 00:48:46 +0530

    Use environment variable for port in app.run
    Update app to use environment variable for port configuration.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
 
@@ -10,4 +11,5 @@ def home():
     return render_template("index.html", output=user_input)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
