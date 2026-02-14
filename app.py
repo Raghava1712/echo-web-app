c7c73d7785cad04e0832500e385268f28c7b3afd
Yendluri Raghava <167185543+Raghava1712@users.noreply.github.com>
2026-02-15 00:36:49 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    user_input = ""
+    if request.method == "POST":
+        user_input = request.form.get("user_input")
+    return render_template("index.html", output=user_input)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
